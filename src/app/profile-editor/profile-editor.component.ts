a968024e79d2a9ec1cdc10b4cd950ceaf3a6760b
ValimGustavo <gustavo.souza.valim@gmail.com>
2020-04-22 12:17:57 -0300

    1 file modified, 2 added

--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -8,7 +8,13 @@ import { FormGroup, FormControl } from '@angular/forms'
 })
 export class ProfileEditorComponent implements OnInit {
 /*
-  FormGroup são uma forma de conectar varios 
+  FormGroup são uma forma de conectar varios formControl. Isto é, mudanças em um desses controls ecoará para os outros formControl presentes na lista.
+
+  Existem duas formas de se fazer um FormGroup, através do constructor:
+
+    um objeto: forma estatica de se declarar um FormGroup. O conjunto de formControl não será alterado.
+
+    Array: forma dinamica de se declarar um FormGroup. Nele, pode ocorrer adições ou remoções de formControls.
 
 
  */
